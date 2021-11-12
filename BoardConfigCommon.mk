52e9d8f6076e2cbe6a3a1238c2ce4751c4a5abbc
Rohan <rohan@rohanpls.com>
2021-11-12 09:29:06 +0530

    disable qcom sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,7 +196,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2021-09-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+#include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
